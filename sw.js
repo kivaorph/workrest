9dcb27a73abfbd2a351d97a9ec2f0e54240cbafa
kivaorph <kai620565@gmail.com>
2025-06-20 18:09:59 +0800

    修复: 点击通知打开新窗口问题，改为聚焦已存在窗口

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ self.addEventListener('notificationclick', (event) => {
                 }
                 return client.focus();
             }
-            return clients.openWindow('/');
+            return clients.openWindow('./');
         })
     );
 }); 
